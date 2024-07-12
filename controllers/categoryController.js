8cc79566513e3fb55aeb87cc5a07a2c75226d70c
pirates59 <lenishaghimire07@gmail.com>
2024-07-12 09:55:46 +0545

    Category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -5,16 +5,9 @@ const getCategories = async (req, res) => {
     const category = await prisma.category.findMany({
       include: { items: true },
     });
-    // if(req.query.search){
-    //     const filterItem = category.filter(category=>category. category_name.includes(req.query.search));
-    //     return res
-    //     .status(201)
-    //     .json({ filterItem });
-    //   }
-
     return res.status(201).json({ category });
   } catch (error) {
-    return res.status(500).json({ error: "Failed to fetch the items !" });
+    return res.status(500).json({ error: "Failed to fetch the items!" });
   }
 };
 
@@ -22,33 +15,38 @@ const addCategory = async (req, res) => {
   try {
     const { category_name } = req.body;
     if (!category_name) {
-      return res.status(501).json({ error: "provide the necessary data!" });
+      return res.status(400).json({ error: "Please provide the category name!" });
     }
-    const addData = await prisma.category.create({
-      data: req.body,
+
+    const existingCategory = await prisma.category.findUnique({
+      where: { category_name },
     });
-    return res
-      .status(201)
-      .json({ message: "Successfully added the category 1", addData });
+
+    if (existingCategory) {
+      return res.status(400).json({ error: "Category already exists!" });
+    }
+
+    const addData = await prisma.category.create({
+      data: { category_name },
+    });
+
+    return res.status(201).json({ message: "Successfully added the category", addData });
   } catch (error) {
-    return res.status(500).json({ error: "Failed to add the category !" });
+    return res.status(500).json({ error: "Failed to add the category!" });
   }
 };
 
 const deleteCategory = async (req, res) => {
   try {
     const cate_id = req.params.id;
-    console.log(cate_id);
     const deleteData = await prisma.category.delete({
       where: {
         category_id: Number(cate_id),
       },
     });
-    return res
-      .status(201)
-      .josn({ message: "Successfully Deleted the category !" });
+    return res.status(201).json({ message: "Successfully Deleted the category!" });
   } catch (error) {
-    return res.status(500).json({ error: "Failed to delete the category !" });
+    return res.status(500).json({ error: "Failed to delete the category!" });
   }
 };
 
