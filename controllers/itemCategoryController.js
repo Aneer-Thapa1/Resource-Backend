5f2cd92e96747b248b38e3dcbd42f6b5ff8d3870
pirates59 <lenishaghimire07@gmail.com>
2024-07-23 15:55:57 +0545

    ItemCategory

--- controllers/itemCategoryController.js
+++ controllers/itemCategoryController.js
@@ -1,15 +1,26 @@
 const prisma = require("../prismaClient");
+
+// Function to add a new item category
 const addItemCategory = async (req, res) => {
   try {
+
+
+    // Extract item_category_name from the request body
     const { item_category_name } = req.body;
+
+     // Check if item_category_name is provided
     if (!item_category_name) {
       return res.status(501).json({ error: "Provide the necessary data!" });
     }
 
+
+    // Check if the item category already exists in the database
     const existingItemCategory = await prisma.itemCategory.findUnique({
       where: { item_category_name },
     });
 
+
+    // Compare item category names in uppercase to handle case sensitivity
     if (existingItemCategory) {
       const upperCategory = item_category_name.toUpperCase();
       const upperExistingCategory = existingItemCategory.item_category_name.toUpperCase();
@@ -18,7 +29,9 @@ const addItemCategory = async (req, res) => {
         return res.status(400).json({ error: "Item category already exists!" });
       }
     }
+  
 
+    // Create a new item category if it does not exist
     const addData = await prisma.itemCategory.create({
       data: {
         item_category_name: item_category_name,
@@ -31,6 +44,7 @@ const addItemCategory = async (req, res) => {
   }
 };
 
+// Function to get all item categories with their associated items
 
 const getItemCategory = async (req, res) => {
   try {
@@ -53,10 +67,14 @@ const deleteItemCategory = async (req, res) => {
         item_category_id: Number(cate_id),
       },
     });
+
+    // Send success response after deletion
     return res
       .status(200)
       .json({ message: "successfully deleted the item category !" });
   } catch (error) {
+
+     // Return error response if an exception occurs
     return res.status(501).json({ error: "failed to delete the category !" });
   }
 };
