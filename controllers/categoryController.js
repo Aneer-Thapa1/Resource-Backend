86cb5fa14567f79e1c88ffddd4f872b299921072
MR. Subi <bantawasubin@gmail.com>
2024-08-21 15:29:05 +0545

    category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -49,24 +49,89 @@ const addCategory = async (req, res) => {
   }
 };
 
-const deleteCategory = async (req, res) => {
+const editCategory = async (req, res) => {
   try {
-    const cate_id = req.params.id;
-    const deleteData = await prisma.category.delete({
-      where: {
-        category_id: Number(cate_id),
-      },
-    });
-    return res
-      .status(201)
-      .json({ message: "Successfully Deleted the category!" });
+    const {type}  = req.body.type;
+    if (type === "Category") {
+      const id = Number(req.params.id);
+      const name = req.body;
+
+      const category = await prisma.category.findFirst({
+        where: {
+          category_id: id,
+        },
+      });
+
+      if (!category)
+        return res.status(400).json({ error: "Category not found" });
+
+      const data = await prisma.category.update({
+        where: {
+          category_id: id,
+        },
+        data: {
+          category_name: name,
+        },
+      });
+      return res.status(200).json({message:"category updated successfully !"});
+    }
+    else if (type == "ItemCategory") {
+      const id = Number(req.params.id);
+      const name = req.body;
+
+      const itemCategory = await prisma.itemCategory.findFirst({
+        where: {
+          item_category_id: id,
+        },
+      });
+
+      if (!itemCategory)
+        return res.status(400).json({ error: " Item Category not found" });
+
+      const data = await prisma.itemCategory.update({
+        where: {
+          item_category_id: id,
+        },
+        data: {
+          item_category_id:name,
+        },
+      });
+      return res.status(200).json({message:"Item Category updated successfully !"});
+    }
+    else if (type == "Feature") {
+      const id = Number(req.params.id);
+      const name = req.body;
+
+      const feature = await prisma.feature.findFirst({
+        where: {
+          feature_id: id,
+        },
+      });
+
+      if (!feature)
+        return res.status(400).json({ error: "Feature not found" });
+
+      const data = await prisma.feature.update({
+        where: {
+          feature_id: id,
+        },
+        data: {
+          feature_name:name,
+        },
+      });
+      return res.status(200).json({message:"Feature updated successfully !"});
+    }
+    else{
+      return res.status(400).json({error:"invalid Type!"});
+
+    }
   } catch (error) {
-    return res.status(500).json({ error: "Failed to delete the category!" });
+    console.log(error);
+    return res.status(500).json({ error: "Internal Sever Error!" });
   }
 };
-
 module.exports = {
   addCategory,
   getCategories,
-  deleteCategory,
+  editCategory,
 };
